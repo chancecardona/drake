d035d399fe73ccd46fa36bdc49da0522fc0b3ab8
Chance Cardona <chance.cardona@picknik.ai>
2023-08-09 10:58:51 -0600

    Added dynamics constraints

--- cartpole_mpc.py
+++ cartpole_mpc.py
@@ -1,7 +1,9 @@
+#!/bin/env python3
+
 import numpy as np
 from pydrake.math import eq
-from pydrake.multibody.plant import MultiBodyPlant
-from pydrake.multibody.tree import SpatialInertia, PlanarJoint
+from pydrake.multibody.plant import MultibodyPlant
+from pydrake.multibody.tree import SpatialInertia, PlanarJoint, RevoluteJoint, PrismaticJoint
 from pydrake.solvers import MathematicalProgram, Solve
 import matplotlib.pyplot as plt
 import time
@@ -29,7 +31,7 @@ def main():
     # Let cart slide around in xy axis
     plant.AddJoint(
         PrismaticJoint(
-            "xy", 
+            "cart_slide", 
             plant.world_frame(), 
             cart.body_frame(),
             [1, 0, 0]
@@ -37,7 +39,7 @@ def main():
     )
     plant.AddJoint(
         RevoluteJoint(
-            "pole_axis",
+            "pole_to_cart",
             cart.body_frame(),
             pole.body_frame(),
             [0, 0, 1]
@@ -46,15 +48,42 @@ def main():
     plant.Finalize()
 
     prog = MathematicalProgram()
+    N = 100
     q = prog.NewContinuousVariables(N, plant.num_positions(), "q") # x (m)
     qd = prog.NewContinuousVariables(N, plant.num_velocities(), "qd") # v (m/s)
     qdd = prog.NewContinuousVariables(N, 1, "qdd") # a (m/s/s) (force is only applied to base of cart)
 
     # Position Constraints
-    prog.AddBoundingBoxConstraint(-100, 100, q[:, 0])
-    prog.AddBoundingBoxConstraint(-100, 100, q[:, 1])
+    #prog.AddBoundingBoxConstraint(-100, 100, q[:, 0])
+    #prog.AddBoundingBoxConstraint(-100, 100, q[:, 1])
     # Force Constraints
     prog.AddBoundingBoxConstraint(-10, 10, qdd)
+
+    # Dynamic Constraints
+    #q[:, 0] is x
+    #q[:, 1] is theta
+    for i in range(1, N):
+        # x dot (velocity)
+        prog.AddConstraint(
+            eq(q[i], 
+               q[i-1] + qd[i-1] * dt
+            )
+        )
+        # x dot dot
+        prog.AddConstraint(
+            eq(qd[i][0], 
+               qd[i-1][0] + 1 / (cart.get_mass() + pole.get_mass() * math.pow(math.sin(q[i-1][1], 2))) * 
+                 (qdd[i-1][0] + pole.get_mass() * math.sin(q[i-1][1]) * (pole.get_length() * math.pow(qd[i-1][1], 2) + g * math.cos(q[i-1][1])))
+            )
+        )
+        # theta dot dot
+        prog.AddConstraint(
+            eq(qd[i][1], 
+               qd[i-1][1] + 1 / (pole.get_length() * (cart.get_mass() + pole.get_mass() * math.pow(math.sin(q[i-1][1]), 2)) ) *
+                 (-qdd[i-1][0] * math.cos(q[i-1][1]) - pole.get_mass() * pole.get_length() * math.pow(qd[i-1][1], 2) * math.cos(q[i-1][1]) * math.sin(q[i-1][1]) - (cart.get_mass() + pole.get_mass()) * g * math.sin(q[i-1][1]))
+            )
+        )
+    print(q)
 
 if __name__ == '__main__':
     main()
